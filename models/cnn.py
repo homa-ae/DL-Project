bbc5129cf73d30b1446ac98a90ded67c365df74d
NoeCoursi <n.coursimaux@gmail.com>
2025-07-22 13:56:54 +0200

    trying new cnn

--- models/cnn.py
+++ models/cnn.py
@@ -1,3 +1,23 @@
+# import torch.nn as nn
+
+# class CNN(nn.Module):
+#     def __init__(self, input_shape, num_classes):
+#         super().__init__()
+#         self.conv = nn.Sequential(
+#             nn.Conv2d(1, 32, kernel_size=3, padding=1),
+#             nn.ReLU(),
+#             nn.MaxPool2d(2),
+#             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+#             nn.ReLU(),
+#             nn.AdaptiveAvgPool2d((1, 1))
+#         )
+#         self.fc = nn.Linear(64, num_classes)
+
+#     def forward(self, x):
+#         x = self.conv(x)
+#         x = x.view(x.size(0), -1)
+#         return self.fc(x)
+
 import torch.nn as nn
 
 class CNN(nn.Module):
@@ -5,15 +25,25 @@ class CNN(nn.Module):
         super().__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),       # <- Ajout BatchNorm
             nn.ReLU(),
             nn.MaxPool2d(2),
+
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),       # <- Ajout BatchNorm
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.AdaptiveAvgPool2d((1, 1))
         )
-        self.fc = nn.Linear(64, num_classes)
+        self.dropout = nn.Dropout(0.5)  # <- Ajout Dropout pour régularisation
+        self.fc = nn.Linear(128, num_classes)
 
     def forward(self, x):
         x = self.conv(x)
         x = x.view(x.size(0), -1)
+        x = self.dropout(x)
         return self.fc(x)
