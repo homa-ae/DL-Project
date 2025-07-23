9a1181508c4a458166b7e98096c9b9e4060a50f0
NoeCoursi <n.coursimaux@gmail.com>
2025-07-23 19:26:41 +0200

    nice run

--- config.py
+++ config.py
@@ -6,13 +6,14 @@ config = {
     "audio_normalize": True, 
     "feature_normalize": False,
     "num_speakers": 10,
-    "max_length_per_speaker": 60.0,  # max duration for every speaker in second
+    "max_length_per_speaker": 180.0,  # max duration for every speaker in second
     "segment_duration": 3.0,
-    "batch_size": 32,
-    "num_epochs": 10,
+    "batch_size": 16,
+    "num_epochs": 20,
     "learning_rate": 1e-3,
     "model_type": "resnet",    # or "rnn", "crnn"
-    "k_folds": 2,
+    "k_folds": 3,
+    "HPO": True,
     "train_vol": 0.6,       # train+val+test should be 1
     "val_vol": 0.2, 
     "test_vol": 0.2,
